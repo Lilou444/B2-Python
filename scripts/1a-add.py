a39640c9f9017583a1e9fa414b3a88bf1eb8d302
root <root@localhost.localdomain>
2018-11-05 21:13:25 +0100

    init repos

---
+++ scripts/1a-add.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env python3
+ 
+def addition():
+    nb1 = int(input("Entrez le premier nombre : "))
+    nb2 = int(input("Entrez le deuxieme nombre : "))
+    return nb1 + nb2
+ 
+print(addition()) #on affiche la fonction 
+
+
