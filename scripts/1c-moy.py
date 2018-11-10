473f8b871268468a5fe44e266b2a652b063b8c20
root <root@localhost.localdomain>
2018-11-11 00:14:20 +0100

    init repos

--- scripts/1c-moy.py
+++ scripts/1c-moy.py
@@ -2,23 +2,27 @@
 
 ###1c-moy
 ### Lilou444
-### 2018-11-05 (derniere mise à jour)
+### 11-11-2018 (derniere mise à jour)
 
-def moyenne() : 
+def stockage_de_note() : 
    mylist = []
    prenom = ' leila '
    Moy = 0
    note = 0
+   total=0
    while(note>=0) and (prenom != 'q'):
        prenom = input("entrez prenoms ")
        note=int(input('Entrez les notes : '))
        if (note>=0) and (prenom != 'q'):
            note=float(note)
-           mylist.append(note)
-           Moy = sum(mylist) / len(mylist)
-           print(Moy)
+           mylist.append(note)     #on stocke les notes dans une liste 
+       for note in mylist:
+            total += note       #on additionne toutes les notes de cette liste
+            Moy = total / len(mylist)   #on fait la moyenne 
+            print(Moy)
+           
+      
+   
+print(stockage_de_note())
 
-print(moyenne())
-
-#je dois l'ameliorer car il fait la moyenne juste pour un élève 
-#j'espère avoir compris l'idée 
+ 
