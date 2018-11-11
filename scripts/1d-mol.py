fee095bc2bcfe3e4b65d26230855acc5a0695827
root <root@localhost.localdomain>
2018-11-11 14:27:53 +0100

    init repos

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -4,23 +4,29 @@
 ### Lilou444
 ### 2018-11-05 (derniere mise à jour)
 
+
+##Jeux plus ou moins 
+
+
 from random import *
 
 
-def moyenne ():
-nombre = random() 
-nb = int(input("entrez un nombre"))
-while nombre != nb :
-    nb = (input("entrez un nombre"))
-    if (nb > nombre) and (nb< nombre):
-        print(nombre)
-    if nb == 'q':
-        return none 
+def moyenne():
+    nombre = random()
+    nb = int(input("entrez un nombre"))
+    while [nombre != nb]:
+        nb = (int(input("entrez un nombre")))
+        if (nb > nombre) :
+            print("mon nombre est plus grand")
+        if (nb < nombre) :
+            print("mon nombre est plus petit")
+        if (nb == 'q'):
+            return none 
 
 
 def affichage():
     print("au revoir")
-    print(moyenne())
+    print(nombre)
 
 
-
+print(ffichage())
